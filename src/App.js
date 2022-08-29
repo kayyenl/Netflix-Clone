d315ad082101bb16bf1943fdae2ea58b5764a14b
kayyenl <yenyenlee9@gmail.com>
2022-08-29 23:24:48 +0800

    making lots of small components for the jumbotron

--- src/App.js
+++ src/App.js
@@ -5,7 +5,17 @@ import jumboData from './fixtures/jumbo.json'
 export default function App() {
   return (
     <Jumbotron.Container>
-      {jumboData.map((item) => )}
-    <Jumbotron.Container />
+      {jumboData.map((item) => (
+        <Jumbotron direction={item.direction} key={item.id}>
+          <Jumbotron.Pane>
+            <Jumbotron.Title>{item.title}</Jumbotron.Title>
+            <Jumbotron.subTitle>{item.subTitle}</Jumbotron.subTitle>
+          </Jumbotron.Pane>
+          <Jumbotron.Pane>
+            <Jumbotron.Image src={item.image} alt={item.alt}/>
+          </Jumbotron.Pane>
+        </Jumbotron>
+      ))}
+    </Jumbotron.Container>
   );
 }
--- src/components/jumbotron/styles/Jumbotron.jsx
+++ src/components/jumbotron/styles/Jumbotron.jsx
@@ -14,3 +14,16 @@ export const Inner = styled.div`
         flex-direction: column;
     }
 `
+
+export const Container = styled.div``
+export const Pane = styled.div`
+    width: 50%;
+`
+export const Title = styled.h1`
+
+`
+
+export const SubTitle = styled.h2`
+
+`
+export const Image = styled.image``
