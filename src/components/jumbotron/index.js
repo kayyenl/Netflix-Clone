ff6a194266ef861c08b3d72b48506acbff43962e
kayyenl <yenyenlee9@gmail.com>
2022-08-29 15:40:51 +0800

    going to begin the build today

--- src/components/jumbotron/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function Jumbotron() {
-    return (
-        <p>This is the Jumbotron!</p>
-    )
-}
--- src/components/jumbotron/styles/Jumbotron.jsx
+++ src/components/jumbotron/styles/Jumbotron.jsx
@@ -1,4 +1,9 @@
 import styled from 'styled-components/macro';
 
 
-export const Inner = styled.div
+export const Inner = styled.div`
+    display: flex;
+    align-content: center;
+    justify-content: space-between;
+    max-width: 1100px;
+`
